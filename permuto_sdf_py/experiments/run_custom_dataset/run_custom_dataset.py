93139068bb38a93b3ea3070c696943da0c4d7d31
ezxr.tangxiaotian <tangxiaotian@ezxr.com>
2023-06-13 15:23:43 +0800

    my cutom dataset with colmap coordinate

--- permuto_sdf_py/experiments/run_custom_dataset/run_custom_dataset.py
+++ permuto_sdf_py/experiments/run_custom_dataset/run_custom_dataset.py
@@ -23,7 +23,8 @@ from permuto_sdf_py.train_permuto_sdf import train
 from permuto_sdf_py.train_permuto_sdf import HyperParamsPermutoSDF
 import permuto_sdf_py.paths.list_of_training_scenes as list_scenes
 
-
+import json
+import cv2
 
 torch.manual_seed(0)
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
@@ -35,89 +36,89 @@ parser.add_argument('--dataset_path', default="/media/rosu/Data/data/permuto_sdf
 parser.add_argument('--with_mask', action='store_true', help="Set this to true in order to train with a mask")
 parser.add_argument('--exp_info', default="", help='Experiment info string useful for distinguishing one experiment for another')
 parser.add_argument('--no_viewer', action='store_true', help="Set this to true in order disable the viewer")
-parser.add_argument('--scene_scale', default=0.9, type=float, help='Scale of the scene so that it fits inside the unit sphere')
-parser.add_argument('--scene_translation', default=[0,0,0], type=float, nargs=3, help='Translation of the scene so that it fits inside the unit sphere')
-parser.add_argument('--img_subsample', default=1.0, type=float, help="The higher the subsample value, the smaller the images are. Useful for low vram")
+parser.add_argument('--scene_scale', default=0.25, type=float, help='Scale of the scene so that it fits inside the unit sphere')
+parser.add_argument('--scene_translation', default=[0.0861233,0.428559, 0.125612], type=float, nargs=3, help='Translation of the scene so that it fits inside the unit sphere')
+parser.add_argument('--img_subsample', default=2., type=float, help="The higher the subsample value, the smaller the images are. Useful for low vram")
+parser.add_argument('--img_folder', default="/workspace/permuto_sdf/permuto_sdf_py/data/fuwa/images", type=str, help='')
+parser.add_argument('--mask_folder', default="/workspace/permuto_sdf/permuto_sdf_py/data/fuwa/mask", type=str, help='')
+parser.add_argument('--img_json', default="/workspace/permuto_sdf/permuto_sdf_py/data/fuwa/images.json", type=str, help='')
+parser.add_argument('--coorid', default="cv", type=str, help='cv or gl')
 args = parser.parse_args()
 with_viewer=not args.no_viewer
-
 
 #MODIFY these for your dataset!
 SCENE_SCALE=args.scene_scale
 SCENE_TRANSLATION=args.scene_translation
 IMG_SUBSAMPLE_FACTOR=args.img_subsample #subsample the image to lower resolution in case you are running on a low VRAM GPU. The higher this number, the smaller the images
 DATASET_PATH=args.dataset_path #point this to wherever you downloaded the easypbr_data (see README.md for download link)
- 
+IMG_PATH = args.img_folder
+MASK_PATH = args.mask_folder
+IMG_JSON = args.img_json
+
 def create_custom_dataset():
-    #CREATE CUSTOM DATASET---------------------------
-    #We need to fill easypbr.Frame objects into a list. Each Frame object contains the image for a specific camera together with extrinsics and intrinsics
-    #intrinsics and extrinsics
-    assert os.path.exists(DATASET_PATH), "The dataset path does not exist. Please point to the path where you downloaded the EasyPBR renders"
-
-
-    intrinics_extrinsics_file=os.path.join(DATASET_PATH,"poses_and_intrinsics.txt")
-    with open(intrinics_extrinsics_file) as file:
-        lines = [line.rstrip() for line in file]
-    #remove comments
-    lines = [item for item in lines if not item.startswith('#')]
-    #images
-    path_imgs=os.path.join(DATASET_PATH,"imgs_train") #modify this to wherever your 
-    imgs_names_list=[img_name for img_name in os.listdir(path_imgs)]
-    imgs_names_list=natsort.natsorted(imgs_names_list,reverse=False)
-
-    #create list of frames for this scene
+    info = json.load(open(IMG_JSON))
+    K=np.identity(3)
+    K[0][0]=info['fl_x'] #fx
+    K[1][1]=info['fl_y'] #fy
+    K[0][2]=info['cx'] #cx
+    K[1][2]=info['cy'] #cy
+    imgs_names_list = info['frames']
+    path_imgs = IMG_PATH
     frames=[]
-    for idx, img_name in enumerate(imgs_names_list):
-        #load img as single precision RGB
+    for idx, img in enumerate(imgs_names_list):
+        img_name = img['file_name']
         print("img_name", img_name)
+        transform_matrix = np.array(img['transform_matrix'])
         frame=Frame()
-        img=Mat(os.path.join(path_imgs,img_name))
+        img=Mat(os.path.join(path_imgs, img_name))
         img=img.to_cv32f()
         #get rgb part and possibly the alpha as a mask
         if img.channels()==4:
             img_rgb=img.rgba2rgb()
         else:
             img_rgb=img
-        #get the alpha a a mask if necessary
-        if args.with_mask and img.channels()==4:
-            img_mask=img.get_channel(3)
-            frame.mask=img_mask
-        if args.with_mask and not img.channels()==4:
-            exit("You are requiring to use a foreground-background mask which should be in the alpha channel of the image. But the image does not have 4 channels")
-        frame.rgb_32f=img_rgb 
-
-        #img_size
         frame.width=img.cols
         frame.height=img.rows
-
-        #intrinsics as fx, fy, cx, cy
-        calib_line=lines[idx]
-        calib_line_split=calib_line.split()
-        K=np.identity(3)
-        K[0][0]=calib_line_split[-4] #fx
-        K[1][1]=calib_line_split[-3] #fy
-        K[0][2]=calib_line_split[-2] #cx
-        K[1][2]=calib_line_split[-1] #cy
+        if args.with_mask:
+            img_mask = cv2.imread(os.path.join(MASK_PATH, img_name), cv2.IMREAD_GRAYSCALE)
+            if img_mask.shape[0] != img.rows or img_mask.shape[1] != img.cols:
+                img_mask = cv2.resize(img_mask, (img.cols, img.rows))
+            frame.mask=img_mask
+        frame.rgb_32f=img_rgb 
         frame.K=K
 
-        #extrinsics as a tf_cam_world (transformation that maps from world to camera coordiantes)
-        translation_world_cam=calib_line_split[1:4] #translates from cam to world
-        quaternion_world_cam=calib_line_split[4:8] #rotates from cam to world
-        tf_world_cam=Affine3f()
-        tf_world_cam.set_quat(quaternion_world_cam) #assumes the quaternion is expressed as [qx,qy,qz,qw]
-        tf_world_cam.set_translation(translation_world_cam)
-        tf_cam_world=tf_world_cam.inverse() #here we get the tf_cam_world that we need
-        frame.tf_cam_world=tf_cam_world
+        # #extrinsics as a tf_cam_world (transformation that maps from world to camera coordiantes)
+        # translation_world_cam=calib_line_split[1:4] #translates from cam to world
+        # quaternion_world_cam=calib_line_split[4:8] #rotates from cam to world
+        # tf_world_cam=Affine3f()
+        # tf_world_cam.set_quat(quaternion_world_cam) #assumes the quaternion is expressed as [qx,qy,qz,qw]
+        # tf_world_cam.set_translation(translation_world_cam)
+        # tf_cam_world=tf_world_cam.inverse() #here we get the tf_cam_world that we need
+        if args.coorid == "gl":
+            pass
+        elif args.coorid == "cv":
+            flip_mat = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+		    ])
+            transform_matrix = np.matmul(transform_matrix, flip_mat)
+            transform_matrix = np.linalg.inv(transform_matrix)
+            # transform_matrix[1, :] = -transform_matrix[1, :]
+            # transform_matrix[2, :] = -transform_matrix[2, :]
+        tf_cam_world = Affine3f()
+        tf_cam_world.from_matrix(transform_matrix)
+        frame.tf_cam_world = tf_cam_world
         #ALTERNATIVELLY if you have already the extrinsics as a numpy matrix you can use the following line
         # frame.tf_cam_world.from_matrix(YOUR_4x4_TF_CAM_WORLD_NUMPY_MATRIX) 
 
         #scale scene so that the object of interest is within a sphere at the origin with radius 0.5
-        tf_world_cam_rescaled = frame.tf_cam_world.inverse()
-        translation=tf_world_cam_rescaled.translation().copy()
+        # tf_world_cam_rescaled = frame.tf_cam_world.inverse()
+        translation=tf_cam_world.translation().copy()
+        translation-=SCENE_TRANSLATION
         translation*=SCENE_SCALE
-        translation+=SCENE_TRANSLATION
-        tf_world_cam_rescaled.set_translation(translation)
-        frame.tf_cam_world=tf_world_cam_rescaled.inverse()
+        frame.tf_cam_world.set_translation(translation)
 
         #subsample the image to lower resolution in case you are running on a low VRAM GPU
         frame=frame.subsample(IMG_SUBSAMPLE_FACTOR)
